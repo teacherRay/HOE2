6666415f30b26a4d6c953439655ad935e2accc6c
teacherRay <34903301+teacherRay@users.noreply.github.com>
2018-07-03 20:57:06 +0700

    Got about screen buttons going

--- app/src/main/java/com/example/ray/hoe2/AboutActivity.kt
+++ app/src/main/java/com/example/ray/hoe2/AboutActivity.kt
@@ -1,12 +1,33 @@
 package com.example.ray.hoe2
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class AboutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        val btnNewsScreen : Button = findViewById(R.id.buttonNews)
+        btnNewsScreen.setOnClickListener{
+            val intent = Intent(this,NewsActivity :: class.java)
+            startActivity(intent)
+        }
+
+        val btnFeesScreen : Button = findViewById(R.id.buttonFees)
+        btnFeesScreen.setOnClickListener{
+            val intent = Intent(this,FeesActivity :: class.java)
+            startActivity(intent)
+        }
+        val btnMapScreen : Button = findViewById(R.id.buttonMap)
+        btnMapScreen.setOnClickListener{
+            val intent = Intent(this,MapActivity :: class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
