f7913f7da1281efbeedc8c72afba15b48fda10a4
teacherRay <34903301+teacherRay@users.noreply.github.com>
2018-07-03 19:28:20 +0700

    Added Map Screen

--- app/src/main/java/com/example/ray/hoe2/MapActivity.kt
+++ app/src/main/java/com/example/ray/hoe2/MapActivity.kt
@@ -1,12 +1,40 @@
 package com.example.ray.hoe2
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+
+        mMap = googleMap
+
+        mMap.setMinZoomPreference(16.0f)
+        mMap.setMaxZoomPreference(16.0f)
+
+        // Add a marker in Home of English and move the camera
+        val hoe = LatLng(11.548259, 104.914899)
+        mMap.addMarker(MarkerOptions().position(hoe).title("Home of English"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(hoe))
+    }
+
+
 }
+
