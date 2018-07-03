504b8dfe68b251fdbf3fce44384e81db083b5e63
teacherRay <34903301+teacherRay@users.noreply.github.com>
2018-07-03 21:10:06 +0700

    Got about screen buttons going

--- app/src/main/java/com/example/ray/hoe2/NewsActivity.kt
+++ app/src/main/java/com/example/ray/hoe2/NewsActivity.kt
@@ -1,12 +1,21 @@
 package com.example.ray.hoe2
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class NewsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+
+        val btnPrevScreen: Button = findViewById(R.id.buttonBack)
+        btnPrevScreen.setOnClickListener{
+            val intent = Intent(this, AboutActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
