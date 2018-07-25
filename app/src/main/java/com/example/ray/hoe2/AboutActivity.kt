bdb8ab7cc9aa1d7057e0d40804f1b8ad90df2748
teacherRay <34903301+teacherRay@users.noreply.github.com>
2018-07-25 18:57:32 +0700

    pre delete buttons menus already added to about screen

--- app/src/main/java/com/example/ray/hoe2/AboutActivity.kt
+++ app/src/main/java/com/example/ray/hoe2/AboutActivity.kt
@@ -3,7 +3,11 @@ package com.example.ray.hoe2
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import kotlinx.android.synthetic.main.activity_about.*
+
 
 class AboutActivity : AppCompatActivity() {
 
@@ -33,7 +37,59 @@ class AboutActivity : AppCompatActivity() {
             val intent = Intent(this,CalActivity :: class.java)
             startActivity(intent)
         }
+
+
+        }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        super.onCreateOptionsMenu(menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.about_menu_item) {
+            showAbout()
+        } else if (item.itemId == R.id.map_menu_item) {
+            showMap()
+        } else if (item.itemId == R.id.contact_menu_item) {
+            showContact()
+        } else if (item.itemId == R.id.news_menu_item) {
+            showNews()
+        } else if (item.itemId == R.id.fees_menu_item) {
+            showFees()
+        }
+        return true
     }
 
 
-}
+        private fun showAbout(){
+            val intent = Intent(this,AboutActivity :: class.java)
+            startActivity(intent)
+        }
+
+        public fun showMap(){
+            val intent = Intent(this,MapActivity :: class.java)
+            startActivity(intent)
+        }
+
+        public fun showContact(){
+            val intent = Intent(this,FeesActivity :: class.java)
+            startActivity(intent)
+        }
+        public fun showNews(){
+            val intent = Intent(this,NewsActivity :: class.java)
+            startActivity(intent)
+        }
+        public fun showFees(){
+            val intent = Intent(this,FeesActivity :: class.java)
+            startActivity(intent)
+        }
+    }
+
+
+
+
+
+
+
